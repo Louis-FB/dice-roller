f5a463b46811df32ca8a275622d50f96dd0f225c
louis <louisbart3@gmail.com>
2024-06-12 22:50:24 +0200

    display

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 
 int randomNumber();
+void printDice(int side);
 
 int main() {
 
@@ -12,7 +13,7 @@ int main() {
 		if (choice == 'n')
 			break;
 
-		std::cout << randomNumber();
+		printDice(randomNumber());
 	}
 
 	return 0;
@@ -25,4 +26,40 @@ int randomNumber() {
 
 	return rand() % 8;
 
+}
+
+void printDice(int side) { // show ascii art of die
+	switch (side) {
+	case 1:
+		std::cout << "One (test)";
+		break;
+	case 2:
+		std::cout << "Two (test)";
+		break;
+	case 3:
+		std::cout << "Three (test)";
+		break;
+	case 4:
+		std::cout << "Four (test)";
+		break;
+	case 5:
+		std::cout << "Five (test)";
+		break;
+	case 6:
+		std::cout << "Six (test)";
+		break;
+	case 7:
+		std::cout << "Seven (test)";
+		break;
+	case 8:
+		std::cout << "Eight (test)";
+		break;
+	case 9:
+		std::cout << "Nine (test)";
+		break;
+	default:
+		std::cout << "???";
+		break;
+	}
+	
 }
